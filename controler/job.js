5ec6798c75148e5470f7823254a8f55c4c888987
Saurabh Shrikhande <saurabhshrikhande@200gmail.com>
2024-03-07 02:12:18 +0530

    post method  complete, data save in db

--- controler/job.js
+++ controler/job.js
@@ -1,21 +1,30 @@
 const jobmodel = require("../model/job")   //need for // insert data in db
 
-const createjob =  (req, res)=>{
-      console.log(req.body);
+const createjob = async (req, res)=>{
+    try{
+        console.log(req.body);
       
-      // insert data in db
-      // insert 2 step other .findmany .delete one step
-      
-           // for save  new 
-         //  new jobmodel.(req.body);
-
-         const newjob = new jobmodel(req.body);
-        const newlyaddeddata =  newjob.save()   // which is a method
+        // insert data in db
+        // insert 2 step other .findmany .delete one step
         
-      res.json({
+             // for save  new 
+           //  new jobmodel.(req.body);
+  
+           const newjob = new jobmodel(req.body);
+          const newlyaddeddata =  await newjob.save()   // which is a method
+           
+
+        res.json({
+              sucess : true,
+              message : `dummy create api ${newlyaddeddata._id}`
+          })
+    }  catch (err){  
+        res.json({
             sucess : true,
-            message : `dummy create api ${newlyaddeddata._id}`
+            message : `dummy create api ${err}`
         })
+    }
+    
     }
 
 
